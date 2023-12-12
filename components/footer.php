ab63feb00af9eb12b0b5ed11bd470d1ab9900861
unknown <yonuxbot@gmail.com>
2023-12-12 17:27:59 +0700

    feat: footer design

--- components/footer.php
+++ components/footer.php
@@ -3,7 +3,47 @@
         <path fill="currentColor" d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"></path>
     </svg>
 
-    <div class="w-full bg-[#FF9130] h-64">
-        <!-- Your content goes here -->
+    <div class="w-full bg-[#FF9130] h-auto flex flex-col px-4 md:px-16 gap-6">
+        <div class="flex flex-col md:flex-row md:justify-between gap-4 md:pt-16">
+            <div class="flex flex-row mt-8 gap-4">
+                <img class="w-16 h-16 bg-[#22092C] rounded-md" src="./assets/images/icon.png">
+                <div class="flex flex-col">
+                    <p class="text-4xl font-bold text-[#22092C]">AnterKuy</p>
+                    <p class="text-[#22092C] font-bold text-sm">Murah, Aman, Cepat, dan Terpercaya.</p>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-6">
+                <div class="flex flex-col ">
+                    <p class="text-[#22092C] font-bold">Fitur</p>
+                    <p class="text-[#22092C]">Pengiriman</p>
+                    <p class="text-[#22092C]">Review</p>
+                    <p class="text-[#22092C]">Refund</p>
+                    <p class="text-[#22092C]">Rewards</p>
+                </div>
+
+                <div class="flex flex-col">
+                    <p class="text-[#22092C] font-bold">Perusahaan</p>
+                    <p class="text-[#22092C]">Karir</p>
+                    <p class="text-[#22092C]">Pusat bantuan</p>
+                    <p class="text-[#22092C]">Media</p>
+                </div>
+
+                <div class="flex flex-col">
+                    <p class="text-[#22092C] font-bold">Kontak</p>
+                    <p class="text-[#22092C]">Jakarta Barat</p>
+                    <p class="text-[#22092C]">Indonesia</p>
+                    <p class="text-[#22092C]">0885 - 8932 - 0931</p>
+                    <p class="text-[#22092C] break-words">support@anterkuy.id</p>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="flex w-full mt-auto mb-2 justify-center items-center">
+            <p class="font-bold text-[#22092C] text-xs md:text-lg">
+                2023 © AnterKuy • All Rights reserved • Made in Love 💖
+            </p>
+        </div>
     </div>
 </div>
