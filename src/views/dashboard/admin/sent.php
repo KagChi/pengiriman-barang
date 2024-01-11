36b054e38a90a9fecb9dc7fbcde99636e26b23d2
unknown <yonuxbot@gmail.com>
2024-01-11 21:40:37 +0700

    fix: remove pagination

--- src/views/dashboard/admin/sent.php
+++ src/views/dashboard/admin/sent.php
@@ -117,21 +117,6 @@ include "./src/components/head.php";
                                     </tbody>
                                 </table>
                             </div>
-                            <div class="py-1 px-4">
-                                <nav class="flex items-center space-x-1">
-                                    <button type="button" class="p-2.5 inline-flex items-center gap-x-2 text-sm rounded-full text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                                        <span aria-hidden="true">«</span>
-                                        <span class="sr-only">Previous</span>
-                                    </button>
-                                    <button type="button" class="min-w-[40px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2.5 text-sm rounded-full disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10" aria-current="page">1</button>
-                                    <button type="button" class="min-w-[40px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2.5 text-sm rounded-full disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10">2</button>
-                                    <button type="button" class="min-w-[40px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2.5 text-sm rounded-full disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10">3</button>
-                                    <button type="button" class="p-2.5 inline-flex items-center gap-x-2 text-sm rounded-full text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                                        <span class="sr-only">Next</span>
-                                        <span aria-hidden="true">»</span>
-                                    </button>
-                                </nav>
-                            </div>
                         </div>
                     </div>
                 </div>
